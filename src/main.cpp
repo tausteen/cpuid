f45cf59c30837456677b0c31f6670267b5b18c3b
Thor Audun Steen <tasn@datarespons.no>
2023-08-28 10:09:04 +0200

    Update main with more cpu info

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,11 @@
 int main(int argc, char **argv)
 {
     cpuinfo_initialize();
-    printf("Running on %s CPU\n", cpuinfo_get_package(0)->name);
+    std::cout << "----------------------------------- CPU INFO -----------------------------------" << std::endl;
+    std::cout << std::left << std::setw(20) << " model:";
+    std::cout << cpuinfo_get_package(0)->name << std::endl;
+    std::cout << std::left << std::setw(20) << " physical cores:";
+    std::cout << cpuinfo_get_package(0)->core_count << std::endl;
+    std::cout << std::left << std::setw(20) << " logical cores:";
+    std::cout << cpuinfo_get_package(0)->processor_count << std::endl;
 }
